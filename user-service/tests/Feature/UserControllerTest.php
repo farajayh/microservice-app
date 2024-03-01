ec44bc3a2dde271da433ef3da9192a7cb66f1da8
Ifara Joshua <farajayh@gmail.com>
2024-03-01 12:16:55 +0100

    update

--- user-service/tests/Feature/UserControllerTest.php
+++ user-service/tests/Feature/UserControllerTest.php
@@ -41,4 +41,54 @@ class UserControllerTest extends TestCase
         ]);            
     }
 
+
+
+    /**
+     * Test that email is required
+     *
+     * @return void
+     */
+    public function test_email_is_required()
+    {
+        $userData = [
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+        ];
+
+        $this->json('POST', 'api/user', $userData)
+            ->assertStatus(400);            
+    }
+
+    /**
+     * Test that first name is required
+     *
+     * @return void
+     */
+    public function test_firstName_is_required()
+    {
+        $userData = [
+            'email' => 'test@example.com',
+            'lastName' => 'Doe',
+        ];
+
+        $this->json('POST', 'api/user', $userData)
+            ->assertStatus(400);            
+    }
+
+    /**
+     * Test that lastName is required
+     *
+     * @return void
+     */
+    public function test_lastName_is_required()
+    {
+        $userData = [
+            'email' => 'test@example.com',
+            'firstName' => 'John',
+        ];
+
+        $this->json('POST', 'api/user', $userData)
+            ->assertStatus(400);            
+    }
+
 }
