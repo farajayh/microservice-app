69a5730ebbbee938207e127a279daa304a3d23f1
Ifara Joshua <farajayh@gmail.com>
2024-03-01 08:44:52 +0100

    update

---
+++ user-service/tests/Feature/UserControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * Test successful creation of a user.
+     *
+     * @return void
+     */
+    public function testUserCreationSuccess()
+    {
+        $userData = [
+            'email' => 'test@example.com',
+            'firstName' => 'John',
+            'lastName' => 'Doe',
+        ];
+
+        $this->json('POST', 'api/user', $userData)
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+                'message' => 'Success',
+        ]);            
+    }
+
+}
